88e00a3470dc5306601a690643c317aac955ac39
Abhijith S <abhijith4389@gmail.com>
2026-05-15 10:43:19 -0900

    api updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-baseURL:"https://full-stack-habit-tracker-fd2t.onrender.com"
+baseURL:"https://habit-tracker-hd4b.onrender.com"
 })
 
 export default API;
