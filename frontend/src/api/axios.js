550ae4945c68c32549767cd9393d6dd915d13739
Abhijith S <abhijith4389@gmail.com>
2026-05-15 10:20:17 -0900

    api updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-baseURL: import.meta.env.VITE_API_URL
+baseURL:"https://full-stack-habit-tracker-fd2t.onrender.com"
 })
 
 export default API;
